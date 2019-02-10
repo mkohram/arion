739cf2ace36f04f143030caed172c2f45991699b
Mojtaba Kohram <mkohram@gmail.com>
2019-02-10 12:05:46 -0500

    adding csvs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='arion',
+      version='0.1',
+      description='arion utils',
+      author='Mojtaba Kohram',
+      author_email='mkohram@gmail.com',
+      packages=['arion'],
+      zip_safe=False)
